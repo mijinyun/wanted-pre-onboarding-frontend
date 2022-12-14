47dbad3680fed1823177b80afc60733a1b0a09a6
mijinyun <107825260+mijinyun@users.noreply.github.com>
2022-12-15 00:55:21 +0900

    feat:메인페이지 scss 완료

--- task/src/component/main/Main.js
+++ task/src/component/main/Main.js
@@ -1,13 +1,16 @@
 import { useNavigate } from 'react-router-dom';
+import './Main.scss';
 
 const Main = () => {
     const navigate = useNavigate();
 
     return (
-        <>
-            <button type='button' onClick={() => navigate('/signIn')}>로그인하러가기</button>
-            <button type='button' onClick={() => navigate('/signUp')}>회원가입하러가기</button>
-        </>
+        <div className='main_entire_section'>
+            <div className='btn_section'>
+                <button type='button' onClick={() => navigate('/signIn')}>로그인하러가기</button>
+                <button type='button' onClick={() => navigate('/signUp')}>회원가입하러가기</button>
+            </div>
+        </div>
     )
 }
 
