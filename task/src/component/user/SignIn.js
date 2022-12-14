19a156fff26f76fccd536f2650263c66a264a615
mijinyun <107825260+mijinyun@users.noreply.github.com>
2022-12-15 01:53:22 +0900

    style: 로그인페이지 스타일 적용완료

--- task/src/component/user/SignIn.js
+++ task/src/component/user/SignIn.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { createRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './SignIn.scss';
 
 const apiURL = 'https://pre-onboarding-selection-task.shop/';
 
@@ -47,27 +48,32 @@ const SignIn = () => {
 
             console.log(data);
             if (data.status === 200) {
-                alert('로그인 성공!'); 
+                // alert('로그인 성공!'); 
                 navigate('/todo');
                 localStorage.setItem('access_token',data.data.access_token);
             }
-            else if (data.status === 401) alert('아이디 또는 비밀번호 틀림 or 없는 회원');
+            // else if (data.status === 401) alert('아이디 또는 비밀번호 틀림 or 없는 회원');
 
+        })
+        .catch((err) => {
+            document.querySelector('.warninglogin').innerHTML = `<p style="color:red;">아이디 또는 비밀번호를 확인해주세요.</p>`;
         })
     }
 
     return (
-        <>
+        <div className='signIn_entire_section'>
+            <h1>LOGIN</h1>
+            <div className="warninglogin"></div>
             <form>
-                이메일: <input type="email" ref={email} name="email" onChange={checking}></input>
-                비밀번호: <input type="password" ref={pw} name="pw" onChange={checking}></input>
+                <input type="email" ref={email} name="email" placeholder="이메일을 입력해주세요. (ex)abc@abc.abc" onChange={checking}></input>
+                <input type="password" ref={pw} name="pw" placeholder='비밀번호를 입력해주세요.(8자이상)' onChange={checking}></input>
                 <button 
                     type='button' 
                     className="loginBtn" 
                     disabled = {loginInfo.validEmail && loginInfo.validPW == true ? false : true}
                     onClick={login}>로그인</button>
             </form>
-        </>
+        </div>
     )
 }
 
